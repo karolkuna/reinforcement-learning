3dc8380336fe89ddd63730b461aa9209e3dae259
karolkuna <karolkuna@me.com>
2017-03-14 00:32:21 +0100

    multi-step amc

--- amc.py
+++ amc.py
@@ -9,6 +9,9 @@ from layers import ScalarMultiplyLayer, AdditionLayer
 
 class AMC:
     def __init__(self, actor_network, model_network, reward_network, value_network, forward_steps=1, discount_factor=0.9, batch_size=128, replay_buffer_size=100000, learning_rate=0.0001, actor_target_approach_rate=0.999, value_target_approach_rate=0.999):
+        if forward_steps < 1:
+            raise Exception("At least one forward step has to be executed!")
+
         self.state_dim = actor_network.input_dims[0]
         self.action_dim = actor_network.output_dim
         self.discount_factor = discount_factor
@@ -32,10 +35,10 @@ class AMC:
         self.value_optimizer = SquaredLossOptimizer(value_network, tf.train.AdamOptimizer(learning_rate))
 
         self.actor_ac_network = self.create_actor_critic_network(
-            "Actor_AC", actor_network, model_network, reward_network, self.value_target_network, 1
+            "Actor_AC", actor_network, model_network, reward_network, self.value_target_network, 1 # TODO: should actor learn from multiple forward steps too?
         )
         self.value_ac_network = self.create_actor_critic_network(
-            "Value_AC", self.actor_target_network, model_network, reward_network, self.value_target_network, 1
+            "Value_AC", self.actor_target_network, model_network, reward_network, self.value_target_network, forward_steps
         )
 
         self.actor_optimizer = MaxOutputOptimizer(
@@ -49,27 +52,37 @@ class AMC:
     def create_actor_critic_network(self, name, actor_network, model_network, reward_network, value_network, forward_steps):
         actor_critic = NeuralNetwork(name, self.actor_network.session, [self.state_dim])
         state_input = actor_critic.get_input_layer(0)
+        discounted_rewards_sum_pred = None
+        current_state = state_input
 
-        actor = actor_network.copy(name + "_actor", reuse_parameters=True)
-        actor.set_input_layer(0, state_input)
-        action_pred = actor.get_output_layer()
+        for step in range(forward_steps):
+            actor = actor_network.copy(name + "_actor_" + str(step), reuse_parameters=True)
+            actor.set_input_layer(0, state_input)
+            action_pred = actor.get_output_layer()
 
-        model = model_network.copy(name + "_model", reuse_parameters=True)
-        model.set_input_layer(0, state_input)
-        model.set_input_layer(1, action_pred)
-        next_state_pred = model.get_output_layer()
+            model = model_network.copy(name + "_model_" + str(step), reuse_parameters=True)
+            model.set_input_layer(0, state_input)
+            model.set_input_layer(1, action_pred)
+            next_state_pred = model.get_output_layer()
+            current_state = next_state_pred
 
-        reward = reward_network.copy(name + "_reward", reuse_parameters=True)
-        reward.set_input_layer(0, state_input)
-        reward.set_input_layer(1, action_pred)
-        reward_pred = reward.get_output_layer()
+            reward = reward_network.copy(name + "_reward_" + str(step), reuse_parameters=True)
+            reward.set_input_layer(0, state_input)
+            reward.set_input_layer(1, action_pred)
+            reward_pred = reward.get_output_layer()
+
+            if step == 0:
+                discounted_rewards_sum_pred = reward_pred
+            else:
+                discounted_reward_pred = ScalarMultiplyLayer("discounted_reward_" + str(step), reward_pred, pow(self.discount_factor, step + 1))
+                discounted_rewards_sum_pred = AdditionLayer("discounted_rewards_sum_" + str(step), [discounted_rewards_sum_pred, discounted_reward_pred])
 
         value = value_network.copy(name + "_value", reuse_parameters=True)
-        value.set_input_layer(0, next_state_pred)
+        value.set_input_layer(0, current_state)
         value_pred = value.get_output_layer()
 
-        discounted_value_pred = ScalarMultiplyLayer("discounted_value", value_pred, self.discount_factor)
-        expected_return = AdditionLayer("expected_return", [discounted_value_pred, reward_pred])
+        discounted_value_pred = ScalarMultiplyLayer("discounted_value", value_pred, pow(self.discount_factor, forward_steps))
+        expected_return = AdditionLayer("expected_return", [discounted_value_pred, discounted_rewards_sum_pred])
 
         actor_critic.compile(expected_return)
         return actor_critic
