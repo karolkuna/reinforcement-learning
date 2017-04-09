b9479d899a982ce18af09cee0496942a258dd88a
Karol Kuna <karolkuna@hotmail.com>
2017-04-09 11:25:28 +0200

    NN class for convenient network creation

---
+++ nn.py
@@ -0,0 +1,45 @@
+# Copyright 2017 Karol Kuna
+
+import tensorflow as tf
+from neuralnetwork import NeuralNetwork
+from layers import *
+
+class NN():
+    @staticmethod
+    def fully_connected(name, session, input_dims, layer_dims, output_dim, activation_fn, output_bounds=None):
+        network = NeuralNetwork(name, session, input_dims)
+        if len(input_dims) == 1:
+            prev_layer = network.get_input_layer(0)
+        else:
+            prev_layer = ConcatLayer("concat_inputs", network.input_layers)
+
+        for i, size in enumerate(layer_dims):
+            layer = FullyConnectedLayer("hidden_" + str(i) + "_" + str(size), size, prev_layer, activation_fn)
+            prev_layer = layer
+
+        output_layer = FullyConnectedLayer("output", output_dim, prev_layer, tf.identity)
+        if output_bounds is not None:
+            output_layer = BoundingLayer("bounding", output_layer, output_bounds.low, output_bounds.high)
+
+        network.compile(output_layer)
+        return network
+
+    @staticmethod
+    def fully_connected_with_bn(name, session, input_dims, layer_dims, output_dim, activation_fn, output_bounds=None):
+        network = NeuralNetwork(name, session, input_dims)
+        if len(input_dims) == 1:
+            prev_layer = network.get_input_layer(0)
+        else:
+            prev_layer = ConcatLayer("concat_inputs", network.input_layers)
+
+        for i, size in enumerate(layer_dims):
+            bn = BatchNormalizationLayer("bn_" + str(i), prev_layer)
+            layer = FullyConnectedLayer("hidden_" + str(i) + "_" + str(size), size, bn, activation_fn)
+            prev_layer = layer
+
+        output_layer = FullyConnectedLayer("output", output_dim, prev_layer, tf.identity)
+        if output_bounds is not None:
+            output_layer = BoundingLayer("bounding", output_layer, output_bounds.low, output_bounds.high)
+
+        network.compile(output_layer)
+        return network
