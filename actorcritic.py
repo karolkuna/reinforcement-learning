d0ff03436cab8f8e76e174e4b0053d6d951feea6
karolkuna <karolkuna@me.com>
2017-04-13 23:19:17 +0200

    creating networks that return td and squared error

--- actorcritic.py
+++ actorcritic.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from neuralnetwork import NeuralNetwork
-from layers import ScalarMultiplyLayer, AdditionLayer
+from layers import ScalarMultiplyLayer, AdditionLayer, SubtractionLayer, SquaredDifferenceLayer, SumLayer
 
 
 def create_actor_critic_network(name, actor_network, critic_network):
@@ -72,3 +72,58 @@ def create_actor_model_critic_network(name, actor_network, model_network, reward
     actor_model_critic.compile(expected_return, [net.get_output_layer() for net in values[:-1]])
     return actor_model_critic, actors, models, rewards, values
 
+
+def create_model_based_td_error_network(name, actor_network, model_network, reward_network, value_network, discount_factor):
+    td_error_network = NeuralNetwork(name, actor_network.session, value_network.input_dims)
+    state_input = td_error_network.get_input_layer(0)
+
+    value = value_network.copy(name + "_value", True)
+    value.set_input_layer(0, state_input)
+    value_pred = value.get_output_layer()
+
+    actor = actor_network.copy(name + "_actor", True)
+    actor.set_input_layer(0, state_input)
+    action_pred = actor.get_output_layer()
+
+    reward = reward_network.copy(name + "_reward", True)
+    reward.set_input_layer(0, state_input)
+    reward.set_input_layer(1, action_pred)
+    reward_pred = reward.get_output_layer()
+
+    model = model_network.copy(name + "_model", True)
+    model.set_input_layer(0, state_input)
+    model.set_input_layer(1, action_pred)
+    next_state_pred = model.get_output_layer()
+
+    next_value = value_network.copy(name + "_next_value", True)
+    next_value.set_input_layer(0, next_state_pred)
+    next_value_pred = next_value.get_output_layer()
+    discounted_value_pred = ScalarMultiplyLayer("discounted_value", next_value_pred, discount_factor)
+    expected_return = AdditionLayer("expected_return", [discounted_value_pred, reward_pred])
+    td_error = SubtractionLayer("td_error", [expected_return, value_pred])
+
+    td_error_network.compile(td_error)
+    return td_error_network
+
+
+def create_squared_error_network(name, network):
+    input_dims = list(network.input_dims)
+    output_dim = network.get_output_layer().get_size()
+    input_dims.append(output_dim)  # target input
+    error_network = NeuralNetwork(name, network.session, input_dims)
+    target = error_network.get_input_layer(len(input_dims) - 1)
+
+    network_copy = network.copy(name + "_" + network.name, True)
+    for i in range(len(network.input_dims)):
+        network_copy.set_input_layer(i, error_network.get_input_layer(i))
+    output_pred = network_copy.get_output_layer()
+
+    squared_diff = SquaredDifferenceLayer("squared_diff", [output_pred, target])
+
+    if output_dim == 1:
+        squared_error_sum = squared_diff
+    else:
+        squared_error_sum = SumLayer("squared_error_sum", squared_diff)
+
+    error_network.compile(squared_error_sum)
+    return error_network
