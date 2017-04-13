354f172a782d25348a865c5ef6301fa69c964fc0
karolkuna <karolkuna@me.com>
2017-04-13 23:16:06 +0200

    layers: moved redundant operations into super init

--- layers.py
+++ layers.py
@@ -5,6 +5,12 @@ import uuid
 from math import sqrt
 
 class Layer:
+    def __init__(self, name, **kwargs):
+        self.id = None
+        self.name = name
+        self.parameters = None
+        self.output = None
+
     def get_id(self):
         return self.id
 
@@ -41,12 +47,9 @@ class Layer:
 
 class InputLayer(Layer):
     def __init__(self, name, size):
-        self.id = None
-        self.name = name
-        self.size = size
-        self.parameters = None
+        Layer.__init__(self, name)
         self.input_layers = []
-        self.output = None
+        self.size = size
 
     def set_input_layer(self, input_layer):
         self.input_layers = [input_layer]
@@ -71,12 +74,9 @@ class InputLayer(Layer):
 
 class FullyConnectedLayer(Layer):
     def __init__(self, name, size, input_layer, activation_fn=tf.nn.relu, weights_init_stddev=1.0, bias_init_stddev=1.0):
-        self.id = None
-        self.name = name
-        self.size = size
-        self.parameters = None
+        Layer.__init__(self, name)
         self.input_layers = [input_layer]
-        self.output = None
+        self.size = size
 
         self.activation_fn = activation_fn
         self.weights_init_stddev = weights_init_stddev
@@ -111,12 +111,9 @@ class FullyConnectedLayer(Layer):
 
 class ConcatLayer(Layer):
     def __init__(self, name, input_layers):
-        self.id = None
-        self.name = name
-        self.size = sum([l.get_size() for l in input_layers])
-        self.parameters = None
+        Layer.__init__(self, name)
         self.input_layers = input_layers
-        self.output = None
+        self.size = sum([l.get_size() for l in input_layers])
 
     def get_parameter_count(self):
         return 0
@@ -135,17 +132,14 @@ class ConcatLayer(Layer):
 class AdditionLayer(Layer):
     def __init__(self, name, input_layers):
         if (len(input_layers) != 2):
-            raise Exception("Addition layers requires 2 inputs!")
+            raise Exception("Addition layer requires 2 inputs!")
 
         if (input_layers[0].get_size() != input_layers[1].get_size()):
             raise Exception("Addition layer inputs must have same dimension!")
 
-        self.id = None
-        self.name = name
+        Layer.__init__(self, name)
         self.size = input_layers[0].get_size()
-        self.parameters = None
         self.input_layers = input_layers
-        self.output = None
 
     def get_parameter_count(self):
         return 0
@@ -163,12 +157,9 @@ class AdditionLayer(Layer):
 
 class ScalarMultiplyLayer(Layer):
     def __init__(self, name, input_layer, scalar):
-        self.id = None
-        self.name = name
+        Layer.__init__(self, name)
         self.size = input_layer.get_size()
-        self.parameters = None
         self.input_layers = [input_layer]
-        self.output = None
         self.scalar = scalar
 
     def get_parameter_count(self):
@@ -187,12 +178,9 @@ class ScalarMultiplyLayer(Layer):
 
 class DropoutLayer(Layer):
     def __init__(self, name, input_layer, keep_probability):
-        self.id = None
-        self.name = name
+        Layer.__init__(self, name)
         self.size = input_layer.get_size()
-        self.parameters = None
         self.input_layers = [input_layer]
-        self.output = None
         self.keep_probability = keep_probability
 
     def get_parameter_count(self):
@@ -212,10 +200,8 @@ class DropoutLayer(Layer):
 
 class BatchNormalizationLayer(Layer):
     def __init__(self, name, input_layer, momentum=0.99, epsilon=0.001, offset=0.0, scale=1.0, moving_mean_init=0.0, moving_variance_init=1.0):
-        self.id = None
-        self.name = name
+        Layer.__init__(self, name)
         self.size = input_layer.get_size()
-        self.parameters = None
         self.is_parameters_owner = None
         self.input_layers = [input_layer]
         self.momentum = momentum
@@ -224,7 +210,12 @@ class BatchNormalizationLayer(Layer):
         self.scale = scale
         self.moving_mean_init = moving_mean_init
         self.moving_variance_init = moving_variance_init
-        self.output = None
+        self.moving_mean = None
+        self.moving_variance = None
+        self.offset_const = None
+        self.scale_const = None
+        self.batch_mean = None
+        self.batch_variance = None
 
     def get_parameter_count(self):
         return 2
@@ -273,12 +264,9 @@ class BatchNormalizationLayer(Layer):
 
 class ClipLayer(Layer):
     def __init__(self, name, input_layer, lower_bound, upper_bound):
-        self.id = None
-        self.name = name
+        Layer.__init__(self, name)
         self.size = input_layer.get_size()
-        self.parameters = None
         self.input_layers = [input_layer]
-        self.output = None
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
 
@@ -309,12 +297,9 @@ class BoundingLayer(Layer):
         :param lower_bound: scalar or list of scalars. Supports broadcasting
         :param upper_bound: scalar or list of scalars. Supports broadcasting
         """
-        self.id = None
-        self.name = name
+        Layer.__init__(self, name)
         self.size = input_layer.get_size()
-        self.parameters = None
         self.input_layers = [input_layer]
-        self.output = None
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
 
