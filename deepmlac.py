2d48960812736d12c967c8b1bedf66e1dc17e23c
karolkuna <karolkuna@me.com>
2017-04-13 23:20:08 +0200

    deepmlac: getting td, reward, and model error

--- deepmlac.py
+++ deepmlac.py
@@ -3,9 +3,9 @@
 import tensorflow as tf
 import numpy as np
 from replaybuffer import ReplayBuffer
-from neuralnetwork import TargetNeuralNetwork
+from neuralnetwork import NeuralNetwork, TargetNeuralNetwork
 from optimizers import SquaredLossOptimizer, MaxOutputOptimizer
-from actorcritic import create_actor_model_critic_network
+import actorcritic as ac
 
 class DeepMLAC:
     def __init__(self, actor_network, model_network, reward_network, value_network, forward_steps=1, discount_factor=0.9, trace_decay=0.9, tf_optimizer=tf.train.AdamOptimizer(0.0001), actor_target_approach_rate=0.999, value_target_approach_rate=0.999):
@@ -33,16 +33,22 @@ class DeepMLAC:
         self.value_target_network = TargetNeuralNetwork(value_network.name + "_target", value_network, value_target_approach_rate)
         self.value_optimizer = SquaredLossOptimizer(value_network, tf_optimizer)
 
-        self.actor_ac_network, _actors, _models, _rewards, _values = create_actor_model_critic_network(
+        self.actor_ac_network, _actors, _models, _rewards, _values = ac.create_actor_model_critic_network(
             "Actor_AC", actor_network, model_network, reward_network, self.value_target_network, self.discount_factor, 1, False # TODO: should actor learn from multiple forward steps too?
         )
-        self.value_ac_network, _actors, self.value_ac_models, self.value_ac_rewards, self.value_ac_values = create_actor_model_critic_network(
+        self.value_ac_network, _actors, self.value_ac_models, self.value_ac_rewards, self.value_ac_values = ac.create_actor_model_critic_network(
             "Value_AC", self.actor_target_network, model_network, reward_network, self.value_target_network, self.discount_factor, forward_steps, True
         )
 
         self.actor_optimizer = MaxOutputOptimizer(
             self.actor_ac_network, tf_optimizer, actor_network.get_parameters()
         )
+
+        self.td_error_network = ac.create_model_based_td_error_network("TD_error", self.actor_target_network,
+            self.model_network, self.reward_network, self.value_network, self.discount_factor
+        )
+        self.model_error_network = ac.create_squared_error_network("Model_error", model_network)
+        self.reward_error_network = ac.create_squared_error_network("Reward_error", reward_network)
 
         self.actor_network.session.run(tf.global_variables_initializer())
 
@@ -102,3 +108,21 @@ class DeepMLAC:
     def action(self, state):
         action = self.actor_network.predict([state])
         return action
+
+    def get_td_error(self, state):
+        return self.get_td_error_batch([state])[0]
+
+    def get_td_error_batch(self, state_batch):
+        return self.td_error_network.predict_batch([state_batch])
+
+    def get_model_error(self, state, action, next_state):
+        return self.get_model_error_batch([state], [action], [next_state])[0]
+
+    def get_model_error_batch(self, state_batch, action_batch, next_state_batch):
+        return np.asarray(self.model_error_network.predict_batch([state_batch, action_batch, next_state_batch])).reshape(-1)
+
+    def get_reward_error(self, state, action, reward):
+        return self.get_reward_error_batch([state], [action], [reward])[0]
+
+    def get_reward_error_batch(self, state_batch, action_batch, reward_batch):
+        return np.asarray(self.reward_error_network.predict_batch([state_batch, action_batch, reward_batch])).reshape(-1)
