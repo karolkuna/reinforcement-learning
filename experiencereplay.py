2ae94306bafadb9925107edf6d1b84feb91cf13c
karolkuna <karolkuna@me.com>
2017-04-13 23:21:47 +0200

    prioritized experience replay for model based agents

--- experiencereplay.py
+++ experiencereplay.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 import replaybuffer as rb
 
 class ExperienceReplay:
@@ -8,7 +9,12 @@ class ExperienceReplay:
         self.replay_buffer = rb.ReplayBuffer(max_size, agent.state_dim, agent.action_dim)
 
     def add_experience(self, state, action, reward, next_state, done):
-        self.replay_buffer.add(state, action, reward, next_state, done)
+        state_vector = np.array(state, ndmin=1)
+        action_vector = np.array(action, ndmin=1)
+        reward_vector = np.array(reward, ndmin=1)
+        next_state_vector = np.array(next_state, ndmin=1)
+
+        self.replay_buffer.add(state_vector, action_vector, reward_vector, next_state_vector, done)
 
     def train_agent(self, batch_size, training_steps=1):
         for i in range(training_steps):
@@ -23,9 +29,14 @@ class PrioritizedExperienceReplay(ExperienceReplay):
         self.last_td_error = 0
 
     def add_experience(self, state, action, reward, next_state, done):
-        self.last_td_error = self.agent.get_td_error(state, action, reward, next_state, done)
+        state_vector = np.array(state, ndmin=1)
+        action_vector = np.array(action, ndmin=1)
+        reward_vector = np.array(reward, ndmin=1)
+        next_state_vector = np.array(next_state, ndmin=1)
+
+        self.last_td_error = self.agent.get_td_error(state_vector, action_vector, reward_vector, next_state_vector, done)
         priority = math.fabs(self.last_td_error)
-        self.replay_buffer.add(state, action, reward, next_state, done, priority)
+        self.replay_buffer.add(state_vector, action_vector, reward_vector, next_state_vector, done, priority)
 
     def update_oldest_priorities(self, count):
         state_batch, action_batch, reward_batch, next_state_batch, done_batch = self.replay_buffer.get_batch_by_ids(range(count))
@@ -35,3 +46,63 @@ class PrioritizedExperienceReplay(ExperienceReplay):
 
     def get_last_td_error(self):
         return self.last_td_error
+
+class ModelBasedPrioritizedExperienceReplay(ExperienceReplay):
+    def __init__(self, agent, max_size):
+        self.agent = agent
+        self.max_size = max_size
+        self.replay_buffer = rb.PrioritizedReplayBuffer(max_size, agent.state_dim, agent.action_dim, parallel=True)
+        self.model_replay_buffer = rb.PrioritizedReplayBuffer(max_size, agent.state_dim, agent.action_dim, parallel=True)
+        self.reward_replay_buffer = rb.PrioritizedReplayBuffer(max_size, agent.state_dim, agent.action_dim, parallel=True)
+        self.last_td_error = 0
+        self.last_model_error = 0
+        self.last_reward_error = 0
+
+    def add_experience(self, state, action, reward, next_state, done):
+        # make sure all inputs are vectors
+        state_vector = np.array(state, ndmin=1)
+        action_vector = np.array(action, ndmin=1)
+        reward_vector = np.array(reward, ndmin=1)
+        next_state_vector = np.array(next_state, ndmin=1)
+
+        self.last_td_error = self.agent.get_td_error(state_vector)
+        self.last_model_error = self.agent.get_model_error(state_vector, action_vector, next_state_vector)
+        self.last_reward_error = self.agent.get_reward_error(state_vector, action_vector, reward_vector)
+
+        self.replay_buffer.add(state_vector, None, None, None, None, math.fabs(self.last_td_error))
+        self.model_replay_buffer.add(state_vector, action_vector, None, next_state_vector, None, self.last_model_error)
+        self.reward_replay_buffer.add(state_vector, action_vector, reward_vector, None, None, self.last_reward_error)
+
+    def update_oldest_priorities(self, count):
+        state_batch, action_batch, reward_batch, next_state_batch, done_batch = self.replay_buffer.get_batch_by_ids(range(count))
+        td_errors = self.agent.get_td_error_batch(state_batch)
+        self.replay_buffer.update_oldest_priorities([math.fabs(td_error) for td_error in td_errors])
+
+        state_batch, action_batch, reward_batch, next_state_batch, done_batch = self.model_replay_buffer.get_batch_by_ids(range(count))
+        model_errors = self.agent.get_model_error_batch(state_batch, action_batch, next_state_batch)
+        self.model_replay_buffer.update_oldest_priorities(model_errors)
+
+        state_batch, action_batch, reward_batch, next_state_batch, done_batch = self.reward_replay_buffer.get_batch_by_ids(range(count))
+        reward_errors = self.agent.get_reward_error_batch(state_batch, action_batch, reward_batch)
+        self.reward_replay_buffer.update_oldest_priorities(reward_errors)
+
+    def get_last_td_error(self):
+        return self.last_td_error
+
+    def get_last_model_error(self):
+        return self.last_model_error
+
+    def get_last_reward_error(self):
+        return self.last_reward_error
+
+    def train_agent(self, batch_size, training_steps=1):
+        for i in range(training_steps):
+            state_batch, action_batch, _r, next_state_batch, _d = self.model_replay_buffer.get_batch(batch_size)
+            self.agent.train_model(state_batch, action_batch, next_state_batch)
+
+            state_batch, action_batch, reward_batch, _ns, _d = self.reward_replay_buffer.get_batch(batch_size)
+            self.agent.train_reward(state_batch, action_batch, reward_batch)
+
+            state_batch, _a, _r, _ns, _d = self.replay_buffer.get_batch(batch_size)
+            self.agent.train_actor(state_batch)
+            self.agent.train_value(state_batch)
