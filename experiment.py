e138f590fb6debb3037c2dea5a174aa0ecebb907
karolkuna <karolkuna@me.com>
2017-04-24 15:05:08 +0200

    experiment: saving all results and model

--- experiment.py
+++ experiment.py
@@ -1,10 +1,17 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import pprint
 from displayframesasgif import display_frames_as_gif
 from movingaverage import MovingAverage
+import imageio
 
 class Experiment:
-    def __init__(self, environment, settings, render_environment=False, render_frequency=60):
+    def __init__(self, path, session, environment, settings, render_environment=False, render_frequency=60):
+        self.path = path
+        self.session = session
+        self.saver = tf.train.Saver()
         self.environment = environment
         self.settings = settings
         self.render_environment = render_environment
@@ -18,9 +25,16 @@ class Experiment:
         self.td_error_history = []
         self.model_error_history = []
         self.reward_error_history = []
+        self.episode_reward_history = []
+        self.episode_duration_history = []
+        self.episode_history = []
 
         self.frames = []
 
+        os.mkdir(self.path)
+        f = open(self.path + "/settings.txt", "w+")
+        pprint.pprint(self.settings, f)
+        f.close()
 
     def record(self, t, state, action, reward, next_state, done, td_error, model_error=0, reward_error=0):
         self.reward_history.append(reward)
@@ -34,39 +48,103 @@ class Experiment:
         self.td_error_history.append(td_error)
         self.model_error_history.append(model_error)
         self.reward_error_history.append(reward_error)
+        self.episode_history.append(len(self.episode_reward_history))
 
         self.episode_reward += reward
         self.episode_duration += 1
 
         if done:
             print("Episode over. Total reward: {}. Solved in {} steps.".format(self.episode_reward, self.episode_duration))
+            self.episode_reward_history.append(self.episode_reward)
+            self.episode_duration_history.append(self.episode_duration)
             self.episode_reward = 0
             self.episode_duration = 0
 
         if self.render_environment and t % self.render_frequency == 0:
             self.frames.append(self.environment.render(mode = 'rgb_array'))
 
-    def print_all_tf_variables(self, session):
+    def print_all_tf_variables(self):
         variables = tf.trainable_variables()
-        values = session.run(variables)
+        values = self.session.run(variables)
 
         for variable, value in zip(variables, values):
             print(variable.name, value)
 
     def display_frames_as_gif(self):
         display_frames_as_gif(self.frames)
+        imageio.mimsave(self.path + "/frames.gif", self.frames)
 
-    def plot_reward(self):
-        return plt.plot(self.reward_history)
+    def plot_reward(self, skip_steps=0):
+        plt.title('Reward in time')
+        plt.xlabel("Time step")
+        plt.ylabel("Reward")
+        plot = plt.plot(self.reward_history[skip_steps:])
+        plt.savefig(self.path + "/reward.png", dpi=300)
+        return plot
 
-    def plot_cumulative_reward(self):
-        return plt.plot(self.cumulative_reward_history)
+    def plot_cumulative_reward(self, skip_steps=0):
+        plt.title('Cumulative reward in time')
+        plt.xlabel("Time step")
+        plt.ylabel("Cumulative reward")
+        plot = plt.plot(self.cumulative_reward_history[skip_steps:])
+        plt.savefig(self.path + "/cumulative_reward.png", dpi=300)
+        return plot
 
-    def plot_td_error(self):
-        return plt.plot(self.td_error_history)
+    def plot_td_error(self, skip_steps=0):
+        plt.title('Absolute TD error in time')
+        plt.xlabel("Time step")
+        plt.ylabel("TD error")
+        plot = plt.plot(self.td_error_history[skip_steps:])
+        plt.savefig(self.path + "/td_error.png", dpi=300)
+        return plot
 
-    def plot_model_error(self):
-        return plt.plot(self.model_error_history)
+    def plot_model_error(self, skip_steps=0):
+        plt.title('Transition model error in time')
+        plt.xlabel("Time step")
+        plt.ylabel("Squared error")
+        plot = plt.plot(self.model_error_history[skip_steps:])
+        plt.savefig(self.path + "/model_error.png", dpi=300)
+        return plot
 
-    def plot_reward_error(self):
-        return plt.plot(self.reward_error_history)
+    def plot_reward_error(self, skip_steps=0):
+        plt.title('Reward model error in time')
+        plt.xlabel("Time step")
+        plt.ylabel("Squared error")
+        plot = plt.plot(self.reward_error_history[skip_steps:])
+        plt.savefig(self.path + "/reward_error.png", dpi=300)
+        return plot
+
+    def plot_episode_duration(self, skip_episodes=0):
+        plt.title('Duration of episodes')
+        plt.xlabel("Episode")
+        plt.ylabel("Duration")
+        plot = plt.plot(self.episode_duration_history[skip_episodes:])
+        plt.savefig(self.path + "/episode_duration.png", dpi=300)
+        return plot
+
+    def plot_episode_reward(self, skip_episodes=0):
+        plt.title('Cumulative reward of episodes')
+        plt.xlabel("Episode")
+        plt.ylabel("Cumulative reward")
+        plot = plt.plot(self.episode_reward_history[skip_episodes:])
+        plt.savefig(self.path + "/episode_reward.png", dpi=300)
+        return plot
+
+    def save(self):
+        df = pd.DataFrame(columns=["t", "episode", "reward", "cumulative_reward", "td_error", "model_error", "reward_error"])
+        df.t = range(len(self.reward_history))
+        df.episode = self.episode_history
+        df.reward = self.reward_history
+        df.cumulative_reward = self.cumulative_reward_history
+        df.td_error = self.td_error_history
+        df.model_error = self.model_error_history
+        df.reward_error = self.reward_error_history
+        df.to_csv(self.path + "/timesteps.csv", index=None, float_format='%.10f')
+
+        df_episodes = pd.DataFrame(columns=["episode", "reward", "duration"])
+        df_episodes.episode = range(len(self.episode_reward_history))
+        df_episodes.reward = self.episode_reward_history
+        df_episodes.duration = self.episode_duration_history
+        df_episodes.to_csv(self.path + "/episodes.csv", index=None, float_format='%.10f')
+
+        self.saver.save(self.session, self.path + "/model.ckpt")
