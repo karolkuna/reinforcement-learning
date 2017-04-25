be3a8a17058841441ad7b326f904f8bfa99cce39
karolkuna <karolkuna@me.com>
2017-04-26 01:05:02 +0200

    Ornstein-Uhlenbeck process noise

---
+++ ounoise.py
@@ -0,0 +1,29 @@
+# --------------------------------------
+# Ornstein-Uhlenbeck Noise
+# Author: Flood Sung
+# Date: 2016.5.4
+# Reference: https://github.com/rllab/rllab/blob/master/rllab/exploration_strategies/ou_strategy.py
+# --------------------------------------
+
+import numpy as np
+import numpy.random as nr
+
+
+class OUNoise:
+    def __init__(self, action_dimension, mu=0, theta=0.15, sigma=0.2, seed=None):
+        self.action_dimension = action_dimension
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
+        self.state = np.ones(self.action_dimension) * self.mu
+        self.reset()
+        nr.seed(seed)
+
+    def reset(self):
+        self.state = np.ones(self.action_dimension) * self.mu
+
+    def noise(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * nr.randn(len(x))
+        self.state = x + dx
+        return self.state
