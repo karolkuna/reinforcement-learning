a193e9f1495c7f1695a52f13b20c0d677b65f887
karolkuna <karolkuna@me.com>
2017-04-29 13:36:00 +0200

    fully connected nn with encoding of equal size for every input

--- nn.py
+++ nn.py
@@ -24,7 +24,40 @@ def fully_connected(name, session, input_dims, layer_dims, output_dim, activatio
         bn = BatchNormalizationLayer("bn_output", prev_layer)
         prev_layer = bn
 
-    output_layer = FullyConnectedLayer("output", output_dim, prev_layer, tf.identity)
+    output_layer = FullyConnectedLayer("output", output_dim, prev_layer, output_activation_fn)
+    if output_bounds is not None:
+        output_layer = BoundingLayer("bounding", output_layer, output_bounds.low, output_bounds.high)
+
+    network.compile(output_layer)
+    return network
+
+
+def fully_connected_with_input_encoding(name, session, input_dims, encoding_dim, layer_dims, output_dim, activation_fn, output_bounds=None, batch_norm=False, output_activation_fn=tf.identity):
+    network = NeuralNetwork(name, session, input_dims)
+    encoding_layers = []
+    for input_id, input_dim in enumerate(input_dims):
+        if batch_norm:
+            input_layer = network.get_input_layer(input_id)
+            prev_layer = BatchNormalizationLayer("bn_encoding_" + str(input_id), input_layer)
+        else:
+            prev_layer = network.get_input_layer(input_id)
+        encoding_layer = FullyConnectedLayer("encoding_" + str(input_id) + "_" + str(encoding_dim), encoding_dim, prev_layer, activation_fn)
+        encoding_layers.append(encoding_layer)
+
+    prev_layer = ConcatLayer("concat_inputs", encoding_layers)
+
+    for i, size in enumerate(layer_dims):
+        if batch_norm:
+            bn = BatchNormalizationLayer("bn_hidden_" + str(i), prev_layer)
+            prev_layer = bn
+
+        layer = FullyConnectedLayer("hidden_" + str(i) + "_" + str(size), size, prev_layer, activation_fn)
+        prev_layer = layer
+
+    if batch_norm:
+        bn = BatchNormalizationLayer("bn_output", prev_layer)
+        prev_layer = bn
+
     output_layer = FullyConnectedLayer("output", output_dim, prev_layer, output_activation_fn)
     if output_bounds is not None:
         output_layer = BoundingLayer("bounding", output_layer, output_bounds.low, output_bounds.high)
